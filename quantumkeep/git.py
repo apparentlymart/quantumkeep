3c36cf489c1d6dc889b540e09d9091031e6e9efc
Martin Atkins <mart@degeneration.co.uk>
2012-05-27 17:18:25 -0700

    Add the beginnings of writable trees, starting with the ability to create an empty subtree.

--- quantumkeep/git.py
+++ quantumkeep/git.py
@@ -22,17 +22,20 @@ TREE_MODE = 0400000
 
 class Tree(object):
 
-    def __init__(self, repo, tree_id):
+    def __init__(self, repo, tree_id=None):
         self.repo = repo
-        base_tree = repo[tree_id]
-        if type(base_tree) is not GitTree:
-            raise ValueError("%r does not represent a tree" % tree_id)
 
         self._tree_items = {}
         self._live_children = {}
 
-        for item_name, item_mode, item_id in base_tree.iteritems():
-            self._tree_items[item_name] = (item_mode, item_id)
+        self._base_tree_id = tree_id
+
+        if tree_id is not None:
+            base_tree = repo[tree_id]
+            if type(base_tree) is not GitTree:
+                raise ValueError("%r does not represent a tree" % tree_id)
+            for item_name, item_mode, item_id in base_tree.iteritems():
+                self._tree_items[item_name] = (item_mode, item_id)
 
     @property
     def parent(self):
@@ -52,6 +55,10 @@ class Tree(object):
             if tree_entry[0] & TREE_MODE:
                 child_tree = Tree(self.repo, tree_entry[1])
                 self._live_children[key] = child_tree
+                try:
+                    del self._tree_items[key]
+                except KeyError:
+                    pass
                 return child_tree
             else:
                 blob = self.repo[tree_entry[1]]
@@ -70,9 +77,51 @@ class Tree(object):
         for key in live:
             yield key
         for key in self._tree_items:
-            if key not in live:
-                yield key
+            yield key
 
     def iteritems(self):
         for key in self.__iter__():
             yield (key, self[key])
+
+    def make_subtree(self, key):
+        child_tree = Tree(self.repo)
+        self._live_children[key] = child_tree
+        try:
+            del self._tree_items[key]
+        except KeyError:
+            pass
+        return child_tree
+
+    def write_to_repo(self):
+        """
+        Write any new blob and tree objects to the underlying git repository
+        and return the id (hex sha1) of the top-level tree object that results.
+        """
+
+        # Don't do anything if we have no live objects,
+        # since that means we've not actually changed anything.
+        if self._base_tree_id is not None and len(self._live_children) == 0:
+            return self._base_tree_id
+
+        git_tree = GitTree()
+
+        # First add the items that haven't changed at all.
+
+        for key, item_def in self._tree_items.iteritems():
+            git_tree.add(key, item_def[0], item_def[1])
+
+        # Now cook and add the live objects
+
+        for key, value in self._live_children.items():
+            if type(value) is Tree:
+                child_tree_id = value.write_to_repo()
+                git_tree.add(key, TREE_MODE, child_tree_id)
+            else:
+                git_blob = GitBlob.from_string(value)
+                self.repo.object_store.add_object(git_blob)
+                git_tree.add(key, 0, git_blob.id)
+
+        self.repo.object_store.add_object(git_tree)
+
+        self._base_tree_id = git_tree.id
+        return self._base_tree_id
--- tests/test_git.py
+++ tests/test_git.py
@@ -5,6 +5,10 @@ from dulwich.repo import MemoryRepo
 from quantumkeep.git import Tree, GitBlob, GitTree, TREE_MODE
 
 
+# An empty tree always hashes to the following
+EMPTY_TREE = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+
+
 class TestGit(unittest.TestCase):
 
     def setUp(self):
@@ -41,3 +45,46 @@ class TestGit(unittest.TestCase):
         ))
         for key, value in tree.iteritems():
             self.assertEqual(value, tree[key])
+
+    def test_make_subtree(self):
+        tree_id = self.make_tree_fixture()
+        tree = Tree(self.repo, tree_id)
+        child_tree = tree.make_subtree("test_make_subtree")
+        child_child_tree = child_tree.make_subtree("another_subtree")
+        new_tree_id = tree.write_to_repo()
+        self.assertEqual(
+            type(tree["test_make_subtree"]), Tree
+        )
+        self.assertEqual(
+            type(tree["test_make_subtree"]["another_subtree"]), Tree
+        )
+        child_git_tree = self.repo[new_tree_id]
+        child_tree_mode, child_tree_id = child_git_tree["test_make_subtree"]
+        child_child_git_tree = self.repo[child_tree_id]
+        child_child_tree_mode, child_child_tree_id = child_child_git_tree["another_subtree"]
+        self.assertEqual(child_child_tree_id, EMPTY_TREE)
+
+    def test_write_to_repo(self):
+        tree_id = self.make_tree_fixture()
+        tree = Tree(self.repo, tree_id)
+
+        # write_to_repo with no changes yields the same tree id
+        self.assertEqual(tree_id, tree.write_to_repo())
+
+        child_tree = tree.make_subtree("blah")
+
+        new_tree_id = tree.write_to_repo()
+
+        # new_tree_id should be different
+        self.assertNotEqual(new_tree_id, tree_id)
+
+        # write_to_repo with no changes yields the same tree id again
+        self.assertEqual(new_tree_id, tree.write_to_repo())
+
+        # New tree should be in the repo
+        new_git_tree = self.repo[new_tree_id]
+        entry = new_git_tree["blah"]
+        self.assertEqual(entry[1], EMPTY_TREE)
+
+        # The empty tree should also be in the repo
+        empty_tree = self.repo[EMPTY_TREE]
